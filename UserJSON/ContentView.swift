327035f3abedd515253a1d4d4d468bf462b0e1ec
Sullivan677 <decarli.sulli@gmail.com>
2020-12-17 11:33:45 +0100

    Initial Commit

---
+++ UserJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserJSON
+//
+//  Created by Sullivan De carli on 17/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserJSON/UserJSONApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserJSONApp.swift
+//  UserJSON
+//
+//  Created by Sullivan De carli on 17/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct UserJSONApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
